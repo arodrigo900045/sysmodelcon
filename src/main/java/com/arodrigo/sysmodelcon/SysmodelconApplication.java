99e04ebae1a55943b20cb5aa74e859ee7398f571
arodrigo900045 <adilsonmissionario@gmail.com>
2021-03-21 22:14:35 +0100

    Projeto Criado

---
+++ src/main/java/com/arodrigo/sysmodelcon/SysmodelconApplication.java
@@ -0,0 +1,14 @@
+package com.arodrigo.sysmodelcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysmodelconApplication 
+{
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(SysmodelconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arodrigo/sysmodelcon/SysmodelconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arodrigo.sysmodelcon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysmodelconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
