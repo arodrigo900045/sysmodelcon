ca29d073bf9f9f94b521332720a40920e7d60914
arodrigo900045 <adilsonmissionario@gmail.com>
2021-03-22 22:14:16 +0100

    Produto e Associação muitos para muitos

--- src/main/java/com/arodrigo/sysmodelcon/domain/Categoria.java
+++ src/main/java/com/arodrigo/sysmodelcon/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.arodrigo.sysmodelcon.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable
@@ -16,6 +19,10 @@ public class Categoria implements Serializable
    @GeneratedValue(strategy=GenerationType.IDENTITY)
    private Integer id;
    private String nome;
+   
+   
+   @ManyToMany(mappedBy="categorias")   
+   private List<Produto> produtos = new ArrayList<>();//Associacao da Categoria com Produtos   
    
    public Categoria() 
    {	   
@@ -47,6 +54,15 @@ public class Categoria implements Serializable
    {
 	   this.nome = nome;
    }
+   public List<Produto> getProdutos() 
+   {
+   	return produtos;
+   }
+
+   public void setProdutos(List<Produto> produtos) 
+   {
+   	this.produtos = produtos;
+   }
 
    @Override
    public int hashCode() 
@@ -74,6 +90,8 @@ public class Categoria implements Serializable
 		   return false;
 	   return true;
     }
+
+
     
    
 }
