7213371b86ba994269100f5525762ad4539cd5c9
arodrigo900045 <adilsonmissionario@gmail.com>
2021-03-24 21:34:52 +0100

    Pedito, EstadoPagamento e Pagamento

--- src/main/java/com/arodrigo/sysmodelcon/SysmodelconApplication.java
+++ src/main/java/com/arodrigo/sysmodelcon/SysmodelconApplication.java
@@ -1,5 +1,6 @@
 package com.arodrigo.sysmodelcon;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +12,20 @@ import com.arodrigo.sysmodelcon.domain.Categoria;
 import com.arodrigo.sysmodelcon.domain.Cliente;
 import com.arodrigo.sysmodelcon.domain.Endereco;
 import com.arodrigo.sysmodelcon.domain.Municipio;
+import com.arodrigo.sysmodelcon.domain.Pagamento;
+import com.arodrigo.sysmodelcon.domain.PagamentoComBoleto;
+import com.arodrigo.sysmodelcon.domain.PagamentoComCartao;
+import com.arodrigo.sysmodelcon.domain.Pedido;
 import com.arodrigo.sysmodelcon.domain.Produto;
 import com.arodrigo.sysmodelcon.domain.Provincia;
+import com.arodrigo.sysmodelcon.domain.enums.EstadoPagamento;
 import com.arodrigo.sysmodelcon.domain.enums.TipoCliente;
 import com.arodrigo.sysmodelcon.repositories.CategoriaRepository;
 import com.arodrigo.sysmodelcon.repositories.ClienteRepository;
 import com.arodrigo.sysmodelcon.repositories.EnderecoRepository;
 import com.arodrigo.sysmodelcon.repositories.MunicipioRepository;
+import com.arodrigo.sysmodelcon.repositories.PagamentoRepository;
+import com.arodrigo.sysmodelcon.repositories.PedidoRepository;
 import com.arodrigo.sysmodelcon.repositories.ProdutoRepository;
 import com.arodrigo.sysmodelcon.repositories.ProvinciaRepository;
 
@@ -36,6 +44,11 @@ public class SysmodelconApplication implements CommandLineRunner
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
 	
 	public static void main(String[] args) 
 	{
@@ -87,6 +100,21 @@ public class SysmodelconApplication implements CommandLineRunner
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2020 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2020 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/04/2021 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 
 }
